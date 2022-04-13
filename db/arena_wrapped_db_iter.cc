c64a1e2dc29d1fae10bd9ee2866b05359e3a3bf2
mytrygithub <30644711+mytrygithub@users.noreply.github.com>
2022-04-13 17:42:57 +0800

    Update arena_wrapped_db_iter.cc
    添加问题

--- db/arena_wrapped_db_iter.cc
+++ db/arena_wrapped_db_iter.cc
@@ -36,7 +36,7 @@ void ArenaWrappedDBIter::Init(
     const SequenceNumber& sequence, uint64_t max_sequential_skip_in_iteration,
     uint64_t version_number, ReadCallback* read_callback, DBImpl* db_impl,
     ColumnFamilyData* cfd, bool expose_blob_index, bool allow_refresh) {
-  auto mem = arena_.AllocateAligned(sizeof(DBIter));
+  auto mem = arena_.AllocateAligned(sizeof(DBIter));      //什么使用场景才能体现出这里的优势呢？
   db_iter_ =
       new (mem) DBIter(env, read_options, ioptions, mutable_cf_options,
                        ioptions.user_comparator, /* iter */ nullptr, version,
